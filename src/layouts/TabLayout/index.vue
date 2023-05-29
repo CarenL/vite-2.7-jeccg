229b26432d0a8533980d6f7068b23c965bfd4be5
CarenL <18069794397@163.com>
2023-05-29 21:51:44 +0800

    feat:修改侧边栏样式

--- src/layouts/TabLayout/index.vue
+++ src/layouts/TabLayout/index.vue
@@ -1,127 +1,143 @@
 <template>
   <div :class="classObj" class="app-wrapper">
-    <div v-if="device === 'mobile' && sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    <div v-if="device === 'mobile' && set.sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
     <sidebar class="sidebar-container" />
-    <div :class="{ hasTagsView: needTagsView }" class="main-container">
-      <div :class="{ 'fixed-header': fixedHeader }">
+    <div :class="{ hasTagsView: set.needTagsView }" class="main-container">
+      <div :class="{ 'fixed-header': set.fixedHeader }">
         <navbar />
-        <tags-view v-if="needTagsView" />
+        <tags-view v-if="set.needTagsView" />
       </div>
       <app-main />
-      <right-panel v-if="showSettings">
+      <right-panel v-if="set.showSettings">
         <settings />
       </right-panel>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref, reactive, computed, watch, watchEffect, onMounted, onBeforeMount, onBeforeUnmount, toRefs } from 'vue';
 import { useStore, useRoute } from '@/utils/useApi';
 import RightPanel from '@/components/RightPanel';
 import { AppMain, Navbar, Settings, Sidebar, TagsView } from './components';
 
-export default {
-  name: 'TabLayout',
-  components: {
-    AppMain,
-    Navbar,
-    RightPanel,
-    Settings,
-    Sidebar,
-    TagsView,
-  },
-  setup() {
-    //https://cn.vuejs.org/api/reactivity-core.html
-    const store = useStore();
-    const set = reactive({
-      sidebar: computed(() => {
-        return store.state.app.sidebar;
-      }),
-      device: computed(() => {
-        return store.state.app.device;
-      }),
-      fixedHeader: computed(() => {
-        return store.state.settings.fixedHeader;
-      }),
-      showSettings: computed(() => {
-        return store.state.settings.showSettings;
-      }),
-      needTagsView: computed(() => {
-        return store.state.settings.tagsView;
-      }),
-      classObj: computed(() => {
-        const obj = {
-          hideSidebar: !set.sidebar.opened,
-          openSidebar: !!set.sidebar.opened,
-          withoutAnimation: set.sidebar.withoutAnimation,
-          mobile: set.device === 'mobile',
-        };
-        return obj;
-      }),
-    });
-    const handleClickOutside = (params) => {
-      store.dispatch('app/closeSideBar', { withoutAnimation: params });
+// export default {
+//   name: 'TabLayout',
+//   components: {
+//     AppMain,
+//     Navbar,
+//     RightPanel,
+//     Settings,
+//     Sidebar,
+//     TagsView,
+//   },
+//   setup() {
+//https://cn.vuejs.org/api/reactivity-core.html
+const store = useStore();
+const set = reactive({
+  sidebar: computed(() => {
+    return store.state.app.sidebar;
+  }),
+  device: computed(() => {
+    return store.state.app.device;
+  }),
+  fixedHeader: computed(() => {
+    return store.state.settings.fixedHeader;
+  }),
+  showSettings: computed(() => {
+    return store.state.settings.showSettings;
+  }),
+  needTagsView: computed(() => {
+    return store.state.settings.tagsView;
+  }),
+  menuStyle: computed(() => {
+    return store.state.settings.menu;
+  }),
+  classObj: computed(() => {
+    const obj = {
+      hideSidebar: !set.sidebar.opened,
+      openSidebar: !!set.sidebar.opened,
+      withoutAnimation: set.sidebar.withoutAnimation,
+      mobile: set.device === 'mobile',
     };
-
-    const route = useRoute();
-    const WIDTH = ref(992);
-
-    watchEffect(() => {
-      if (set.device === 'mobile' && !set.sidebar.opened) {
-        handleClickOutside(false);
-      }
-    });
-
-    watch(
-      () => route,
-      async () => {
-        if (set.device === 'mobile' && set.sidebar.opened) {
-          handleClickOutside(false);
-        }
-      },
-      { immediate: true },
-    );
-
-    const $_isMobile = () => {
-      const rect = document.body.getBoundingClientRect();
-      return rect.width - 1 < WIDTH.value;
-    };
-
-    const $_resizeHandler = () => {
-      if (!document.hidden) {
-        const isMobile = $_isMobile();
-        store.dispatch('app/toggleDevice', isMobile ? 'mobile' : 'desktop');
-
-        if (isMobile) {
-          handleClickOutside(true);
-        }
-      }
-    };
-
-    onMounted(() => {
-      const isMobile = $_isMobile();
-      if (isMobile) {
-        store.dispatch('app/toggleDevice', 'mobile');
-        handleClickOutside(true);
-      }
-    });
-
-    onBeforeMount(() => {
-      window.addEventListener('resize', $_resizeHandler);
-    });
-
-    onBeforeUnmount(() => {
-      window.removeEventListener('resize', $_resizeHandler);
-    });
-
-    return {
-      ...toRefs(set),
-      handleClickOutside,
-    };
-  },
+    return obj;
+  }),
+});
+const handleClickOutside = (params) => {
+  store.dispatch('app/closeSideBar', { withoutAnimation: params });
 };
+
+const route = useRoute();
+const WIDTH = ref(992);
+
+watchEffect(() => {
+  if (set.device === 'mobile' && !set.sidebar.opened) {
+    handleClickOutside(false);
+  }
+});
+
+watch(
+  () => route,
+  async () => {
+    if (set.device === 'mobile' && set.sidebar.opened) {
+      handleClickOutside(false);
+    }
+  },
+  { immediate: true },
+);
+
+const $_isMobile = () => {
+  const rect = document.body.getBoundingClientRect();
+  return rect.width - 1 < WIDTH.value;
+};
+
+const $_resizeHandler = () => {
+  if (!document.hidden) {
+    const isMobile = $_isMobile();
+    store.dispatch('app/toggleDevice', isMobile ? 'mobile' : 'desktop');
+
+    if (isMobile) {
+      handleClickOutside(true);
+    }
+  }
+};
+
+onMounted(() => {
+  const isMobile = $_isMobile();
+  if (isMobile) {
+    store.dispatch('app/toggleDevice', 'mobile');
+    handleClickOutside(true);
+  }
+});
+
+onBeforeMount(() => {
+  window.addEventListener('resize', $_resizeHandler);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', $_resizeHandler);
+});
+
+//导航栏样式
+const sideBarWidth = computed(() => {
+  return set.menuStyle.sideBarWidth + 'px';
+});
+console.log(sideBarWidth);
+//     return {
+//       ...toRefs(set),
+//       handleClickOutside,
+//     };
+//   },
+// };
 </script>
+
+<style lang='scss'>
+#app {
+  .main-container {
+    margin-left: v-bind(sideBarWidth);
+  }
+}
+</style>
 
 <style lang="scss" scoped>
 @import '@/styles/mixin.scss';
